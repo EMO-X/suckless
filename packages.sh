c7e15c7c51cd948c1e7b11c48447be8eed68612f
EMO-X <yahyaechcharqui@proton.me>
2025-04-24 12:50:33 +0100

    Stuff

---
+++ packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script for install all packages needed by X11 and dwm 
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root." 
+   exit 1
+fi
+
+echo "Install X11 packages"
+pacman -S --needed --noconfirm  xorg-server xorg-xinit xf86-input-libinput mesa-utils
+sleep 2
+clear
+echo "Install tools packages"
+pacman -S --noconfirm --needed  sxhkd brightnessctl pamixer picom fzf neovim bluez bluez-utils blueman  
+sleep 2
+clear
+echo "Install fonts"
+pacman -S --noconfirm --needed ttf-font-awesome  ttf-nerd-fonts-symbols ttf-dejavu ttf-liberation noto-fonts
+clear
+
