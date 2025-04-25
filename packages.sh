87528cbc8a2111f503c2b0a5394ebaf600aceefb
Yahya <yahyaechcharqui@proton.me>
2025-04-25 13:29:53 +0100

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -6,6 +6,8 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root." 
    exit 1
 fi
+#in void linux packages called
+#sudo xbps-install base-devel libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel
 
 echo "Install X11 packages"
 pacman -S --needed --noconfirm  xorg-server xorg-xinit xf86-input-libinput mesa-utils
